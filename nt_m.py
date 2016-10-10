bdcd75fcb13dbcefdcbf6efcec73da154426774f
KWJ <kwj1399@gmail.com>
2016-10-10 15:25:09 +0800

    Update nt_m.py

--- nt_m.py
+++ nt_m.py
@@ -1,33 +1,27 @@
-# Copyright (C) 2016 kwj
-# and Telecommunications. www.muzixing.com
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-# implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+'''
+@FileName:
+@Author:KWJ
+@UpdateTime:2016/10/10
+@Introduction:
+'''
+
 
 from __future__ import division
 import copy
 from operator import attrgetter
-from ryu import cfg
 from ryu.base import app_manager
 from ryu.base.app_manager import lookup_service_brick
-from ryu.controller import ofp_event
-from ryu.controller.handler import MAIN_DISPATCHER, DEAD_DISPATCHER
-from ryu.controller.handler import CONFIG_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib import hub
 from ryu.lib.packet import packet
+from ryu.controller import ofp_event
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import arp
+from ryu.lib.packet import ether_types
 import setting
+import redis
 
 class NetworkMonitor(app_manager.RyuApp):
     """
@@ -46,6 +40,9 @@ class NetworkMonitor(app_manager.RyuApp):
         self.stats = {}
         self.port_features = {}
         self.free_bandwidth = {}
+        self.mac_to_port = {}
+        self.ip_to_port = {}
+        self.ipfreebw = {}
         # Start to green thread to monitor traffic and calculating
         # free bandwidth of links respectively.
         self.monitor_thread = hub.spawn(self._monitor)
@@ -73,6 +70,14 @@ class NetworkMonitor(app_manager.RyuApp):
         while True:
             self.stats['flow'] = {}
             self.stats['port'] = {}
+            self._save_ipfreebw(self.free_bandwidth,self.ip_to_port,self.ipfreebw)
+            pool = redis.ConnectionPool(host='127.0.0.1',port=6379,db=0)
+            r = redis.StrictRedis(connection_pool=pool)
+            for key in self.ipfreebw.keys():
+                r.set(key,self.ipfreebw[key])
+            print(self.free_bandwidth)
+            print (self.ip_to_port)
+            print (self.ipfreebw)
             for dp in self.datapaths.values():
                 self.port_features.setdefault(dp.id, {})
                 self._request_stats(dp)
@@ -106,8 +111,10 @@ class NetworkMonitor(app_manager.RyuApp):
         if port_state:
             capacity = port_state[2]
             curr_bw = self._get_free_bw(capacity, speed)
-            self.free_bandwidth[dpid].setdefault(port_no, None)
-            self.free_bandwidth[dpid][port_no] = curr_bw
+            key = (dpid,port_no)
+            if key not in setting.SW_PORT:
+                self.free_bandwidth.setdefault(key, None)
+                self.free_bandwidth[(dpid, port_no)] = curr_bw
         else:
             self.logger.info("Fail in getting port state")
 
@@ -134,7 +141,99 @@ class NetworkMonitor(app_manager.RyuApp):
 
     def _get_period(self, n_sec, n_nsec, p_sec, p_nsec):
         return self._get_time(n_sec, n_nsec) - self._get_time(p_sec, p_nsec)
+    def _save_ipfreebw(self,freebw,ip_port,ipfreebw):
+        for key in ip_port.keys():
+            ipfreebw[ip_port[key]]=freebw[key]
 
+    @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+    def switch_features_handler(self, ev):
+        datapath = ev.msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        # install table-miss flow entry
+        #
+        # We specify NO BUFFER to max_len of the output action due to
+        # OVS bug. At this moment, if we specify a lesser number, e.g.,
+        # 128, OVS will send Packet-In with invalid buffer_id and
+        # truncated packet data. In that case, we cannot output packets
+        # correctly.  The bug has been fixed in OVS v2.1.0.
+        match = parser.OFPMatch()
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
+        self.add_flow(datapath, 0, match, actions)
+
+    def add_flow(self, datapath, priority, match, actions, buffer_id=None):
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+
+        inst = [parser.OFPInstructionActions(ofproto.OFPIT_APPLY_ACTIONS,
+                                             actions)]
+        if buffer_id:
+            mod = parser.OFPFlowMod(datapath=datapath, buffer_id=buffer_id,
+                                    priority=priority, match=match,
+                                    instructions=inst)
+        else:
+            mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
+                                    match=match, instructions=inst)
+        datapath.send_msg(mod)
+
+    @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+    def _packet_in_handler(self, ev):
+        # If you hit this you might want to increase
+        # the "miss_send_length" of your switch
+        if ev.msg.msg_len < ev.msg.total_len:
+            self.logger.debug("packet truncated: only %s of %s bytes",
+                              ev.msg.msg_len, ev.msg.total_len)
+        msg = ev.msg
+        datapath = msg.datapath
+        ofproto = datapath.ofproto
+        parser = datapath.ofproto_parser
+        in_port = msg.match['in_port']
+
+        pkt = packet.Packet(msg.data)
+        eth = pkt.get_protocols(ethernet.ethernet)[0]
+        if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+            # ignore lldp packet
+            return
+        dst = eth.dst
+        src = eth.src
+        dpid = datapath.id
+        self.mac_to_port.setdefault(dpid, {})
+        self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+        # learn a mac address to avoid FLOOD next time.
+        self.mac_to_port[dpid][src] = in_port
+
+        # mac_of_port
+        for p in pkt.get_protocols(arp.arp):
+            key = (dpid, in_port)
+            value = p.src_ip
+            if key not in setting.SW_PORT:
+                self.ip_to_port.setdefault(key, value)
+        if dst in self.mac_to_port[dpid]:
+            out_port = self.mac_to_port[dpid][dst]
+        else:
+            out_port = ofproto.OFPP_FLOOD
+
+        actions = [parser.OFPActionOutput(out_port)]
+
+        # install a flow to avoid packet_in next time
+        if out_port != ofproto.OFPP_FLOOD:
+            match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+            # verify if we have a valid buffer_id, if yes avoid to send both
+            # flow_mod & packet_out
+            if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                return
+            else:
+                self.add_flow(datapath, 1, match, actions)
+        data = None
+        if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+            data = msg.data
+
+        out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                  in_port=in_port, actions=actions, data=data)
+        datapath.send_msg(out)
     @set_ev_cls(ofp_event.EventOFPFlowStatsReply, MAIN_DISPATCHER)
     def _flow_stats_reply_handler(self, ev):
         """
@@ -148,8 +247,8 @@ class NetworkMonitor(app_manager.RyuApp):
         self.flow_speed.setdefault(dpid, {})
         for stat in sorted([flow for flow in body if flow.priority == 1],
                            key=lambda flow: (flow.match.get('in_port'),
-                                             flow.match.get('ipv4_dst'))):
-            key = (stat.match['in_port'],  stat.match.get('ipv4_dst'),
+                                             flow.match.get('eth_dst'))):
+            key = (stat.match['in_port'],  stat.match.get('eth_dst'),
                    stat.instructions[0].actions[0].port)
             value = (stat.packet_count, stat.byte_count,
                      stat.duration_sec, stat.duration_nsec)
@@ -177,9 +276,7 @@ class NetworkMonitor(app_manager.RyuApp):
         """
         body = ev.msg.body
         dpid = ev.msg.datapath.id
-        print(ev.msg.body)
         self.stats['port'][dpid] = body
-        self.free_bandwidth.setdefault(dpid, {})
 
         for stat in sorted(body, key=attrgetter('port_no')):
             port_no = stat.port_no
@@ -288,15 +385,15 @@ class NetworkMonitor(app_manager.RyuApp):
                 for stat in sorted(
                     [flow for flow in bodys[dpid] if flow.priority == 1],
                     key=lambda flow: (flow.match.get('in_port'),
-                                      flow.match.get('ipv4_dst'))):
+                                      flow.match.get('eth_dst'))):
                     print('%016x %8x %17s %8x %8d %8d %8.1f' % (
                         dpid,
-                        stat.match['in_port'], stat.match['ipv4_dst'],
+                        stat.match['in_port'], stat.match['eth_dst'],
                         stat.instructions[0].actions[0].port,
                         stat.packet_count, stat.byte_count,
                         abs(self.flow_speed[dpid][
                             (stat.match.get('in_port'),
-                            stat.match.get('ipv4_dst'),
+                            stat.match.get('eth_dst'),
                             stat.instructions[0].actions[0].port)][-1])))
             print '\n'
 
