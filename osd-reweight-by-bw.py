1c3577f0b52b9111290aad20dfcc9d7f2e211814
KWJ <kwj1399@gmail.com>
2016-10-30 19:37:15 +0800

    Update osd-reweight-by-bw.py

--- osd-reweight-by-bw.py
+++ osd-reweight-by-bw.py
@@ -38,16 +38,25 @@ def get_osd_addr_dic():
             print ("get_osd_addr: osd.%d address is %s" % (osd_num, osd_addr))
     return osd_addr_dic
 
-# get osd and its' bandwidth
-def get_osd_bw_dic():
-    osd_bw_dic = {}
+# get osd and its' bandwidth for <length> times
+def get_osd_bw_dic(dict,length):
     pool = redis.ConnectionPool(host='172.25.1.2', port=6379, db=0)
     r = redis.StrictRedis(connection_pool=pool)
     osd_addr_dic = get_osd_addr_dic()
+
+    #delete key in osd_bw but not in osd_addr_dic
+    if dict:
+        for key in dict.keys():
+            if not osd_addr_dic.has_key(key):
+                del dict[key]
+    #update osd_bw of the osd in osd_addr_dic
     for key in osd_addr_dic.keys():
-        osd_bw = r.get(osd_addr_dic[key])
-        osd_bw_dic[key] = osd_bw
-    return osd_bw_dic
+        if key not in dict:
+            dict[key] = []
+        bw = r.get(osd_addr_dic[key])
+        dict[key].append(bw)
+        if len(dict[key]) > length:
+            dict[key].pop(0)
 
 # calculate osd weight base on osd's bandwith
 def calc_osd_weight(osd_bw_dic):
@@ -66,16 +75,33 @@ def osd_reweight(osd_num,weight):
         print ("ceph osd reweight: calling %s" % cmd)
         out = os.system(cmd)
         print ("osd_reweight: %s" % out)
-#
+
 def main():
+    times = 5
+    osd_bw = {}
     while True:
-        update_osd_dump()
-        osd_bw = get_osd_bw_dic()
-        osd_weight = calc_osd_weight(osd_bw)
+        #update <times> times osd bandwidth,eg:times=5,osd_bw={'0':[1,2,3,4,5],'1':[6,7,8,9,10]}
+        # evertime update's interval is 60s
+        new_osd_bw = {}
+        for i in range(times):
+            update_osd_dump()
+            get_osd_bw_dic(osd_bw,times)
+            time.sleep(60)
+            print(osd_bw)
+        #calculate the average osd_bandwidth <times> times
+        for key in osd_bw.keys():
+            sum = 0.0
+            for i in range(len(osd_bw[key])):
+                sum = sum + float(osd_bw[key][i])
+            new_bw = float(sum/len(osd_bw[key]))
+            new_osd_bw[key] = new_bw
+        print(new_osd_bw)
+        osd_weight = calc_osd_weight(new_osd_bw)
+        print (osd_weight)
         for osd_num in osd_weight.keys():
             osd_reweight(osd_num,osd_weight[osd_num])
-        print("sleeping half an hour...")
-        time.sleep(5)
+        print("sleeping 30 minutes...")
+        time.sleep(1800)
 
 if __name__ == "__main__":
     main()
